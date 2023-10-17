751831f71995819acc52c9fa9d94c9610c48736b
isaacasher97 <isaac.asher97@gmail.com>
2023-10-17 04:10:51 -0400

    carousel indicators removed

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -1,11 +1,5 @@
 <template>
   <div id="carouselExampleCaptions" class="carousel slide">
-  <div class="carousel-indicators">
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="3" aria-label="Slide 4"></button>
-  </div>
   <div class="carousel-inner">
     <div class="carousel-item active">
       <img src="../../public/imgs/dentist-clinic.svg" class="d-block w-100" alt="carouselImg">
